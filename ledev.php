35c2ffcc9593ab0e2485b7396aa1a0c85334778b
paulinedev <pauline.de.vallois@gmail.com>
2018-01-02 16:49:17 +0100

    anim

---
+++ ledev.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Le Dev' </title>
+    <link href="css/reset.css" type="text/css" rel="stylesheet"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet"/>
+
+  </head>
+
+  <body>
+    <header>
+        <!-- MENU -->
+      <nav>
+        <a href="index.html">Menu</a>
+        <a href="flexbox.html">Exo flex</a>
+        <a href="#">Le dev'</a>
+        <a href="#">La pédogogie</a>
+      </nav>      
+    </header>
+
+
+
+    <footer>
+
+    </footer>
+  </body>
+
+</html>
